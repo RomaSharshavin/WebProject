1e4ecd1ef2b2b78b9e5072bebe386b54d8ec1080
Margo <Margo@LAPTOP-MARGO>
2024-10-04 14:09:51 +0300

    Создание страниц и ссылок на них

--- AlTecProject/main/urls.py
+++ AlTecProject/main/urls.py
@@ -3,5 +3,12 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('contact', views.contact, name='contact')
+    path('contact', views.contact, name='contact'),
+    path('faq', views.faq, name='faq'),
+    path('production', views.production, name='production'),
+    path('service', views.service, name='service'),
+    path('news', views.news, name='news'),
+    path('about', views.about, name='about'),
+    path('write', views.write, name='write'),
+    path('sertificate', views.sertificate, name='sertificate'),
 ]
--- AlTecProject/main/views.py
+++ AlTecProject/main/views.py
@@ -7,3 +7,31 @@ def index(request):
 
 def contact(request):
     return render(request, 'main/contact.html')
+
+
+def faq(request):
+    return render(request, 'main/faq.html')
+
+
+def production(request):
+    return render(request, 'main/production.html')
+
+
+def service(request):
+    return render(request, 'main/service.html')
+
+
+def sertificate(request):
+    return render(request, 'main/sertificate.html')
+
+
+def write(request):
+    return render(request, 'main/write.html')
+
+
+def news(request):
+    return render(request, 'main/news.html')
+
+
+def about(request):
+    return render(request, 'main/about.html')
