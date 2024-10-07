53651fb93024e8269c404a8e2979ac2dcd6c4109
RomanSharshavin <sharshavin.roma@gmail.com>
2024-10-07 14:14:04 +0300

    Добавлены картографические метки на странице контакты

--- AlTecProject/main/static/js/map.js
+++ AlTecProject/main/static/js/map.js
@@ -1,15 +1,21 @@
 ymaps.ready(init);
 function init() {
     var myMap = new ymaps.Map("map", {
-        center: [59.939374, 30.433026], // Координаты центра карты (Санкт-Петербург)
-        zoom: 13, // Уровень увеличения
+        center: [57.58239091081727,34.56004699233066], // Координаты центра карты (Санкт-Петербург)
+        zoom: 5, // Уровень увеличения
         controls: ['zoomControl'] // Элементы управления
     });
 
     // Добавление метки
-    var myPlacemark = new ymaps.Placemark([59.939374, 30.433026], {
+    var myPlacemarkSPB = new ymaps.Placemark([59.939374, 30.433026], {
         balloonContent: 'ООО "Ал-Тек"'
     });
 
-    myMap.geoObjects.add(myPlacemark);
+    myMap.geoObjects.add(myPlacemarkSPB);
+
+    var myPlacemarkMosk = new ymaps.Placemark([55.468244,37.574444], {
+        balloonContent: 'Офис в Московской обл.'
+    });
+
+    myMap.geoObjects.add(myPlacemarkMosk);
 }
