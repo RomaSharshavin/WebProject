59b1fab53c1bcb7036f33510fd83d517a01255ae
Margo <Margo@LAPTOP-MARGO>
2024-10-10 01:03:06 +0300

    Удаление хлебной крошки (потом добавим)

--- AlTecProject/main/static/js/breadcrumb.js
+++ AlTecProject/main/static/js/breadcrumb.js
@@ -1,14 +1,1 @@
-<script>
-    const breadcrumb = document.querySelector('.breadcrumb');
-    const path = window.location.pathname.split('/').filter(Boolean); // Получаем путь URL
 
-    let breadcrumbHTML = '<a href="/">Главная</a>'; // Начинаем с главной страницы
-
-    path.reduce((acc, cur, index) => {
-        const href = acc + '/' + cur; // Формируем ссылку
-        breadcrumbHTML += ` &gt; <a href="${href}">${cur}</a>`;
-        return href;
-    }, '');
-
-    breadcrumb.innerHTML = breadcrumbHTML;
-</script>
