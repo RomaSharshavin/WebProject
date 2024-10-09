3ce06a1f57fbf79ca548205464f1993e1e02a97b
Margo <Margo@LAPTOP-MARGO>
2024-10-10 01:34:44 +0300

    Изменение интерфейса страницы Контакты

--- AlTecProject/main/static/js/map.js
+++ AlTecProject/main/static/js/map.js
@@ -1,21 +1,21 @@
 ymaps.ready(init);
 function init() {
     var myMap = new ymaps.Map("map", {
-        center: [57.58239091081727,34.56004699233066], // Координаты центра карты (Санкт-Петербург)
+        center: [57.58239091081727, 34.56004699233066], // Координаты центра карты (Санкт-Петербург)
         zoom: 5, // Уровень увеличения
         controls: ['zoomControl'] // Элементы управления
     });
 
-    // Добавление метки
+    // Добавление метки для Санкт-Петербурга
     var myPlacemarkSPB = new ymaps.Placemark([59.939374, 30.433026], {
         balloonContent: 'ООО "Ал-Тек"'
     });
 
     myMap.geoObjects.add(myPlacemarkSPB);
 
-    var myPlacemarkMosk = new ymaps.Placemark([55.468244,37.574444], {
-        balloonContent: 'Офис в Московской обл.'
-    });
-
-    myMap.geoObjects.add(myPlacemarkMosk);
+    // Удалите следующие строки, чтобы не добавлять метку в Москве
+    // var myPlacemarkMosk = new ymaps.Placemark([55.468244, 37.574444], {
+    //     balloonContent: 'Офис в Московской обл.'
+    // });
+    // myMap.geoObjects.add(myPlacemarkMosk);
 }
