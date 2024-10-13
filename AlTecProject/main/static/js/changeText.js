fc1b221bf0f0cd9c7fb7e57f1b69c2c7ff9f60c3
RomanSharshavin <sharshavin.roma@gmail.com>
2024-10-13 17:43:23 +0300

    Настроено от ображение файлов на странице Партнёры

--- AlTecProject/main/static/js/changeText.js
+++ AlTecProject/main/static/js/changeText.js
@@ -1,12 +1,16 @@
-const texts = JSON.parse(document.getElementById('texts-data').textContent);
+
 let currentIndex = 0;
 
 function changeText(direction) {
-    currentIndex += direction; // Изменяем индекс
+    currentIndex += direction;
     if (currentIndex < 0) {
-        currentIndex = texts.length - 1; // Циклический переход влево
+        currentIndex = texts.length - 1;
     } else if (currentIndex >= texts.length) {
-        currentIndex = 0; // Циклический переход вправо
+        currentIndex = 0;
     }
     document.getElementById('text-content').innerHTML = texts[currentIndex]; // Обновляем текст
+    const imageElement = document.getElementById('partner-image');
+    imageElement.src = images[currentIndex].src; // Обновляем изображение
+    imageElement.style.width = images[currentIndex].width; // Задаем ширину
+    imageElement.style.height = images[currentIndex].height; // Задаем высоту
 }
