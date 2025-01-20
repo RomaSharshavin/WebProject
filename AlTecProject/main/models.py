ade4f8d018d298ccb68db02e0e5176acd91cc9f8
Roman Sharshavin <sharshavin.roma@gmail.com>
2025-01-20 19:07:19 +0300

    Панель администрирования и миграции 1.1

--- AlTecProject/main/models.py
+++ AlTecProject/main/models.py
@@ -3,7 +3,8 @@ from django.db import models
 
 class News(models.Model):
     title = models.CharField('Новость', max_length=50)
-    text = models.TextField('Описание')
+    text = models.TextField('Описание', max_length=400)
+    date_written = models.DateTimeField('Дата написания', auto_now_add=True)
 
     def __str__(self):
         return self.title
